4798d52096cb584667b378aaa70b1497a2d6e288
TheMarpe <martin.peterlin7@gmail.com>
2021-07-23 03:29:46 +0200

    Added const specifier where applicable

--- spi_protocol.h
+++ spi_protocol.h
@@ -36,7 +36,7 @@ typedef struct {
     int payloadOffset;
     int currentPacketIndex;
     // 2 packets can be decoded at a time max
-    SpiProtocolPacket packet[2]; 
+    SpiProtocolPacket packet[2];
 } SpiProtocolInstance;
 
 
@@ -46,35 +46,42 @@ enum SPI_PROTOCOL_RETURN_CODE {
     SPI_PROTOCOL_PAYLOAD_BUFFER_NULL = -2
 };
 
-/*
-*   instance - SpiProtocolInstance pointer;
-*/
+/**
+ * Initializes an instance of spi_protocol library
+ * @param instance SpiProtocolInstance pointer;
+ */
 void spi_protocol_init(SpiProtocolInstance *instance);
 
 
-/*
-*  instance - spi protocol instance pointer
-*  buffer - uint8_t pointer to buffer where packet bytes reside
-*  size - number of bytes to parse (max SPI_PROTOCOL_PAYLOAD_SIZE)
-*
-*  returns: SpiProtocolPacket pointer, NULL if packet wasn't parsed
-*/
-SpiProtocolPacket* spi_protocol_parse(SpiProtocolInstance* instance, uint8_t* buffer, int size);
+/**
+ * Parses a given buffer of bytes and returns a complete packet if enough bytes arrived
+ *
+ * @param instance Spi protocol instance pointer
+ * @param buffer Pointer to buffer where packet bytes reside
+ * @param size Number of bytes to parse (max SPI_PROTOCOL_PAYLOAD_SIZE)
+ *
+ * @returns SpiProtocolPacket pointer, NULL if packet wasn't parsed
+ */
+SpiProtocolPacket* spi_protocol_parse(SpiProtocolInstance* instance, const uint8_t* buffer, int size);
 
 
-/*
-* packet - pointer to SpiProtocolPacket where it will be written
-* payload_buffer - Input buffer with payload data
-* Returns: 0 OK, -1 packet is NULL, -2 payload_buffer is NULL
-*/
-int spi_protocol_write_packet(SpiProtocolPacket* packet, uint8_t* payload_buffer, int size);
-int spi_protocol_write_packet2(SpiProtocolPacket* packet, uint8_t* payload_buffer1, uint8_t* payload_buffer2, int size1, int size2);
+/**
+ * Creates a SpiProtocolPacket from a buffer
+ *
+ * @param packet Pointer to SpiProtocolPacket where it will be written
+ * @param payload_buffer Input buffer with payload data
+ * @returns 0 OK, -1 packet is NULL, -2 payload_buffer is NULL
+ */
+int spi_protocol_write_packet(SpiProtocolPacket* packet, const uint8_t* payload_buffer, int size);
+int spi_protocol_write_packet2(SpiProtocolPacket* packet, const uint8_t* payload_buffer1, const uint8_t* payload_buffer2, int size1, int size2);
 
 
-/*
-* packet - pointer to SpiProtocolPacket where header, crc and tail will be written
-* Returns: 0 OK, -1 packet is NULL
-*/
+/**
+ * Adds beggining, ending and calculates CRC for existing bytes in the given SpiProtocolPacket
+ *
+ * @param packet Pointer to SpiProtocolPacket where header, crc and tail will be written
+ * @returns 0 OK, -1 packet is NULL
+ */
 int spi_protocol_inplace_packet(SpiProtocolPacket* packet);
 
 
